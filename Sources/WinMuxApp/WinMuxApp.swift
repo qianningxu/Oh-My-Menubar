e0316e2b3c407ffe86925e96ec7e266a241b401f
qianningxu <qx20@st-andrews.ac.uk>
2026-09-20 19:30:07 +0100

    Remove menu bar status icon

--- Sources/WinMuxApp/WinMuxApp.swift
+++ Sources/WinMuxApp/WinMuxApp.swift
@@ -26,7 +26,6 @@ final class WinMuxAppDelegate: NSObject, NSApplicationDelegate {
 @main
 struct WinMuxApp: App {
     @NSApplicationDelegateAdaptor(WinMuxAppDelegate.self) var appDelegate
-    @StateObject var viewModel = TrayMenuModel.shared
     @StateObject var messageModel = MessageModel.shared
     @StateObject var shortcutSettingsModel = ShortcutSettingsModel.shared
     @Environment(\.openWindow) var openWindow: OpenWindowAction
@@ -36,7 +35,6 @@ struct WinMuxApp: App {
     }
 
     var body: some Scene {
-        menuBar(viewModel: viewModel)
         getShortcutSettingsWindow(model: shortcutSettingsModel)
             .onChange(of: shortcutSettingsModel.openRequestId) { _ in
                 openShortcutSettingsWindow(openWindow)
